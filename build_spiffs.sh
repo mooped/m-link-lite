5b25ba0c93812716838a40eadc6869c3bb5aa455
Steve Barnett <mooped@gmail.com>
2022-07-16 18:14:58 +0100

    Rework file handling so essential pages are baked in to Flash. File manager is at /files/ and handles SPIFFS file management.

--- build_spiffs.sh
+++ build_spiffs.sh
@@ -1,2 +1,2 @@
-./mkspiffs/mkspiffs -c www -p 256 -b 4096 -s 131072 www.spiffs
+./mkspiffs/mkspiffs -c www -p 256 -b 4096 -s 921600 www.spiffs
 python $IDF_PATH/components/esptool_py/esptool/esptool.py --chip esp8266 --port /dev/ttyS9 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 2MB 0x110000 www.spiffs
